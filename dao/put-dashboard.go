d3d4b0f03022451b4541a1881244c62bfe4b6079
Chamith <chamith.eos@gmail.com>
2023-08-15 09:45:50 +0530

    Update put-dashboard.go

--- dao/put-dashboard.go
+++ dao/put-dashboard.go
@@ -3,7 +3,9 @@ package dao
 import (
 	awsConfig "amazon-cloudwatch/awsConfig"
 	"amazon-cloudwatch/dto"
+	"encoding/json"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 )
 
@@ -14,9 +16,15 @@ func PutDashboard(application dto.Dashboard) (*cloudwatch.PutDashboardOutput, er
 		return nil, err
 	}
 
+	// Convert DashboardBody struct to JSON string
+	dashboardBodyJSON, err := json.Marshal(application.DashboardBody)
+	if err != nil {
+		return nil, err
+	}
+
 	result, err := client.PutDashboard(&cloudwatch.PutDashboardInput{
-		DashboardName: application.DashboardName,
-		DashboardBody: application.DashboardBody,
+		DashboardName: &application.DashboardName,
+		DashboardBody:aws.String(string(dashboardBodyJSON)) ,
 	})
 
 	if err != nil {
